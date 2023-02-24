95501f7e23f918c919e0cc30302358f90c82d2a8
3B117161 <114204288+3B117161@users.noreply.github.com>
2023-02-24 10:56:05 +0800

    Add files via upload

---
+++ My Aarray.java
@@ -0,0 +1,18 @@
+public class MyArray {
+	public static void main(string [] args){
+		int [] data = new int [3];
+		data[0]=1;
+		system.out.println(data[0]);
+	}
+}
+
+class Human{
+	int sex;
+	int height;
+	int weight;
+	
+	public void printHight(){
+		height = 0;
+		system.out.println(height);
+	}
+}
